a3daa1a53cb9d69af6a159b5afe4ef719ef94e03
Alice Hata <alicehata9@gmail.com>
2023-01-06 14:41:11 -0300

    add lesson 3 - anatomia das classes

--- README.md
+++ README.md
@@ -3,6 +3,22 @@
 ### Sintaxe Java
 
 - Anatomia das Classes
+
+  - Declarações de variáveis e métodos
+    As variáveis podem iniciar com letra ou`_,` e pode conter `$_`.
+
+    Usamos a palavra-chave `final` para definir uma instrução final/constante ou não modificável.
+
+    Definimos a estrutura da variável como:
+    Tipo NomeBemDefinido = Atribuição(opcional)
+
+    A estrutura dos métodos é:
+    TipoRetorno, NomeObjetivoNoInfinitivo e Parâmetro(s)(opcional)
+
+  Concat é uma power class, serve para conectar textos.
+
+  Quando não temos retorno definimos o tipo como void, caso exista, indicamos o retorno com a palavra `return`.
+
 - Tipos e variáveis
 - Operadores
 - Métodos
--- src/MyClass.java
+++ src/MyClass.java
@@ -1,5 +1,43 @@
 public class MyClass {
   public static void main(String[] args) {
-    System.out.println("Olá, seja bem-vindo!");
+    String primeiroNome = "Alice";
+    String segundoNome = "Hata";
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
   }
+
+  public static String nomeCompleto(String primeiroNome, String segundoNome) {
+    return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+  }
+
+  /*
+   * private static void test() {
+   * // Variáveis
+   * int numeroUm = 1;
+   * int numero$um = 1;
+   * int numero1 = 1;
+   * int numeroum = 1;
+   * int numbero_long = 1;
+   * int _numero1 = 1;
+   * 
+   * // Uso de final
+   * final String BR = "Brasil";
+   * final double PI = 3.14;
+   * final int ESTADOS_BRASILEIROS = 27;
+   * 
+   * // Tipo NomeBemDefinido = Atribuição(opcional)
+   * String meuNome = "Alice";
+   * int anoFabricacao = 2022;
+   * boolean verdadeira = true;
+   * int idade;
+   * double altura;
+   * 
+   * idade = 23;
+   * altura = 1.62;
+   * 
+   * // TipoRetorno NomeObjetivoNoInfinitivo Parâmetro(s)
+   * // int somar(int numero1, int numero2)
+   * // String formatarCep(long cep)
+   * }
+   */
 }
